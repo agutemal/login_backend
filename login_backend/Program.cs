1c353a2f7b425289a94518cf4a452d09c0fcd04d
Abrahan Guatemal <guatemal.abrahan@gmail.com>
2026-09-06 22:26:33 -0500

    add config on program.css to implement jwk token

--- login_backend/Program.cs
+++ login_backend/Program.cs
@@ -1,17 +1,47 @@
 using login_backend.Db;
 using login_backend.Repositories;
 using login_backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+//Paso 1 Autentication JWT: Configuración Jwt que usa JwtService para generar el token
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IJwtService, JwtService>();
 
+// Paso 2 Autenticacion JWT: Leer la configuración Jwt (misma que usa JwtService para generar el token)
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+// Paso 3 Autentication JWT: Registrar el esquema de autenticación JWT Bearer
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    // Paso 4: Definir CÓMO se valida el token recibido en cada petición
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,       // rechaza el token si ya expiró
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+});
+
+//Conexion a la base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
         connectionString,
@@ -31,7 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+// Paso 4: IMPORTANTE - Authentication SIEMPRE antes de Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
